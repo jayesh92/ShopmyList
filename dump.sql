442745c26d0a2b68505f4e3508da11a44645ab35
jayesh92 <jayesh.lahori.11.8.92@gmail.com>
2013-11-10 08:54:03 +0530

    dump.sql

---
+++ dump.sql
@@ -0,0 +1,160 @@
+-- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: angelhack
+-- ------------------------------------------------------
+-- Server version	5.5.34-0ubuntu0.13.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `angelhack`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `angelhack` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `angelhack`;
+
+--
+-- Table structure for table `friendship`
+--
+
+DROP TABLE IF EXISTS `friendship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friendship` (
+  `friend1` varchar(25) NOT NULL DEFAULT '',
+  `friend2` varchar(25) NOT NULL DEFAULT '',
+  PRIMARY KEY (`friend1`,`friend2`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friendship`
+--
+
+LOCK TABLES `friendship` WRITE;
+/*!40000 ALTER TABLE `friendship` DISABLE KEYS */;
+INSERT INTO `friendship` VALUES ('krans4u','jayesh92');
+/*!40000 ALTER TABLE `friendship` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `listno_details`
+--
+
+DROP TABLE IF EXISTS `listno_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `listno_details` (
+  `listno` int(11) NOT NULL AUTO_INCREMENT,
+  `receiver` varchar(25) DEFAULT NULL,
+  `sender` varchar(25) DEFAULT NULL,
+  `store` varchar(25) DEFAULT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`listno`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `listno_details`
+--
+
+LOCK TABLES `listno_details` WRITE;
+/*!40000 ALTER TABLE `listno_details` DISABLE KEYS */;
+INSERT INTO `listno_details` VALUES (7,'krans4u','jayesh92','dominos','pizza'),(8,'krans4u','jayesh92','Mcd','burger'),(9,'jayesh92','krans4u','d39','pbm'),(10,'krans4u','undecided','Apollo','Hello');
+/*!40000 ALTER TABLE `listno_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `listno_item`
+--
+
+DROP TABLE IF EXISTS `listno_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `listno_item` (
+  `listno` int(11) NOT NULL DEFAULT '0',
+  `item` varchar(25) NOT NULL DEFAULT '',
+  PRIMARY KEY (`listno`,`item`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `listno_item`
+--
+
+LOCK TABLES `listno_item` WRITE;
+/*!40000 ALTER TABLE `listno_item` DISABLE KEYS */;
+INSERT INTO `listno_item` VALUES (7,'behenchod'),(7,'madrchood'),(8,'brownie'),(8,'mcveggie'),(10,'bandage'),(10,'dettol');
+/*!40000 ALTER TABLE `listno_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(25) NOT NULL DEFAULT '',
+  `email` varchar(50) DEFAULT NULL,
+  `mobile` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('jayesh92','jayesh.lahori.11.8.92@gmail.com','9581845730'),('krans4u','kdhamele@gmail.com','0000000000');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_list`
+--
+
+DROP TABLE IF EXISTS `user_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) DEFAULT NULL,
+  `list_no` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_list`
+--
+
+LOCK TABLES `user_list` WRITE;
+/*!40000 ALTER TABLE `user_list` DISABLE KEYS */;
+INSERT INTO `user_list` VALUES (4,'krans4u',7),(5,'krans4u',8),(6,'krans4u',10);
+/*!40000 ALTER TABLE `user_list` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-10  8:52:12
